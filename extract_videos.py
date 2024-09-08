42fb1470fbe2452c1c7a3400e2b1c721aac2c0cc
ja-vou-nick <101760557+vitordj@users.noreply.github.com>
2024-09-08 01:39:23 -0300

    video extraction now works with command line args, excel workbooks and worksheets are now declared in a json file

--- extract_videos.py
+++ extract_videos.py
@@ -2,6 +2,8 @@ import os
 import pandas as pd
 import ffmpeg
 from datetime import datetime
+import argparse
+import json
 
 def cut_resize_mp4(df: pd.DataFrame, input_folder: str, output_folder: str | None = None, resize_to: tuple[int, int] | None = (1080, 1920)) -> None:
     """
@@ -53,6 +55,7 @@ def cut_resize_mp4(df: pd.DataFrame, input_folder: str, output_folder: str | Non
             stream = ffmpeg.output(stream, temp_output_file_path, vf=f'scale={resize_to[0]}:{resize_to[1]}', y=None)  # Add 'y=None' to force overwrite
             ffmpeg.run(stream)
             os.replace(temp_output_file_path, output_file_path)  # Replace the original file with the resized one
+    return output_folder
 
 def process_excel_files(file_sheets: dict[str, list[str]]) -> pd.DataFrame:
     """
@@ -81,26 +84,45 @@ def process_excel_files(file_sheets: dict[str, list[str]]) -> pd.DataFrame:
     
     if data_frames:
         df = pd.concat(data_frames, ignore_index=True)
-        df['t_inicial'] = '00:' + df['t_inicial'].astype(str).str[:5].str.strip()
-        df['t_final'] = '00:' + df['t_final'].astype(str).str[:5].str.strip()
-        df['qr_inicial'] = df['qr_inicial'].astype(int, errors='ignore')
-        # altera a maneira como eu trato os erros de conversão
-        df['qr_final'] = df['qr_final'].astype(int, errors='coerce')
+        df['t_inicial'] = '00:' + df['t_inicial'].str.strip().astype(str).str[:5]
+        df['t_final'] = '00:' + df['t_final'].str.strip().astype(str).str[:5]
+        df['qr_inicial'] = df['qr_inicial'].astype(int, errors='raise')
+        df['qr_final'] = df['qr_final'].astype(int, errors='raise')
         
         assert len(df[(df.t_inicial > df.t_final)]) == 0  # there should not be any case where t_inicial is greater than t_final
-        # garante também que não haja valores nulos de qr_final e qr_inicial
-        assert len(df[(df.qr_inicial.isna()) | (df.qr_final.isna())]) == 0
+        assert len(df[(df.qr_inicial.isna()) | (df.qr_final.isna())]) == 0 # there should not be any case where qr_inicial or qr_final is null
 
         return df
     else:
         raise ValueError("No data frames to concatenate.")
 
-if __name__ == '__main__':
-    file_sheets = {
-        # 'name or path to the excel file': ['sheet1', 'sheet2', ...]
-        'vino fino_tiempos QR_FILTRADO.xlsx': ['GoPro2', 'GoPro4'],
-    }
-    
+def main(args):
+    with open(args.file_sheets_json, 'r') as f:
+        file_sheets = json.load(f)
+    json_path = os.path.dirname(args.file_sheets_json)
+    file_sheets = {os.path.join(json_path, file): sheets for file, sheets in file_sheets.items()}
+    print(file_sheets)
     df = process_excel_files(file_sheets)
-        
-    cut_resize_mp4(df.query("arquivo_video == 'GX010006.mp4'"), input_folder=None, output_folder=None, resize_to=(1080, 1920))
+
+    if args.video_file:
+        df = df.query(f"arquivo_video == '{args.video_file}'")
+    
+    new_folder = cut_resize_mp4(
+        df.iloc[:1],
+        input_folder=args.input_folder,
+        output_folder=args.output_folder,
+        resize_to=(args.resize_width, args.resize_height)
+    )
+    print(f'\n{new_folder}')
+
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser(description="Video Extraction and Processing")
+    parser.add_argument('--file_sheets_json', type=str, required=True, help='Path to JSON file containing file sheets information')
+    parser.add_argument('--video_file', type=str, default=None, help='Name of the video file to process')
+    parser.add_argument('--input_folder', type=str, default=None, help='Input folder containing video files')
+    parser.add_argument('--output_folder', type=str, default=None, help='Output folder for processed videos')
+    parser.add_argument('--resize_width', type=int, default=1080, help='Width to resize the video')
+    parser.add_argument('--resize_height', type=int, default=1920, help='Height to resize the video')
+    
+    args = parser.parse_args()
+    main(args)
